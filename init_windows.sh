afa68f9db394ef34eda87e912a0de7840f5171c7
Patrick Chan <16124030+pchan37@users.noreply.github.com>
2020-02-11 14:54:05 -0500

    Add an init script for windows and unix

---
+++ init_windows.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+read -p 'What is the application name: ' APPNAME
+SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+SCRIPT_DIR="$(cygpath -m $SCRIPT_DIR)"
+
+perl -pi -e s,APPNAME=\"appname\",APPNAME=\"$APPNAME\",g $SCRIPT_DIR/utils/darwin/build.sh
+perl -pi -e s,APPNAME=\"appname\",APPNAME=\"$APPNAME\",g $SCRIPT_DIR/utils/darwin/package.sh
+perl -pi -e s,APPNAME=\"appname\",APPNAME=\"$APPNAME\",g $SCRIPT_DIR/utils/linux/build.sh
+perl -pi -e s,APPNAME=\"appname\",APPNAME=\"$APPNAME\",g $SCRIPT_DIR/utils/linux/package.sh
+perl -pi -e s,APPNAME=\"appname\",APPNAME=\"$APPNAME\",g $SCRIPT_DIR/utils/linux/package/install.sh
+perl -pi -e s,APPNAME=\"appname\",APPNAME=\"$APPNAME\",g $SCRIPT_DIR/utils/linux/package/uninstall.sh
+perl -pi -e s,APPNAME=\"appname\",APPNAME=\"$APPNAME\",g $SCRIPT_DIR/utils/windows/build.sh
+perl -pi -e s,APPNAME=\"appname\",APPNAME=\"$APPNAME\",g $SCRIPT_DIR/utils/windows/package.sh
+
+perl -pi -e x,appname,$APPNAME,g $SCRIPT_DIR/utils/darwin/package/Info.plist
+cd $SCRIPT_DIR/utils/darwin/package && tar -zxf sciter-osx-64.tar.gz && rm sciter-osx-64.tar.gz
