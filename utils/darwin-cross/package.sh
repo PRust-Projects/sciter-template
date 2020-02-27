296830e1bd5a0ec01d3ff7fb8463b521e2ce4031
Patrick Chan <16124030+pchan37@users.noreply.github.com>
2020-02-26 22:32:31 -0500

    Fix typo in darwin-cross package script

--- utils/darwin-cross/package.sh
+++ utils/darwin-cross/package.sh
@@ -9,7 +9,7 @@ APPNAME="appname"
 
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 mkdir -p "/tmp/$APPNAME.app/Contents/{MacOS,Resources}"
-cp "$SCRIPT_DIR/package/Info.plist /tmp/$APPNAME.app/Contents"
+cp "$SCRIPT_DIR/package/Info.plist" "/tmp/$APPNAME.app/Contents"
 cp "$SCRIPT_DIR/../../target/x86_64-apple-darwin/release/$APPNAME" "/tmp/$APPNAME.app/Contents/MacOS"
 cp -r "$SCRIPT_DIR/package/sciter-osx-64.dylib" "/tmp/$APPNAME.app/Contents/MacOS"
 cp "$SCRIPT_DIR/package/$APPNAME.icns" "/tmp/$APPNAME.app/Contents/Resources"
